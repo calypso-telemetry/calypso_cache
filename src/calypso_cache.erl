12e919e9eb72a404f4a86eb1b346ce31d7b7b95b
Sergey Loguntsov <loguntsov@gmail.com>
2015-06-18 15:39:01 +0300

    Change name of author.

--- src/calypso_cache.erl
+++ src/calypso_cache.erl
@@ -1,5 +1,5 @@
 -module(calypso_cache).
--author("begemot").
+-author("Sergey Loguntsov").
 
 %% API
 -export([
--- src/calypso_cache_app.erl
+++ src/calypso_cache_app.erl
@@ -1,5 +1,5 @@
 -module(calypso_cache_app).
--author("begemot").
+-author("Sergey Loguntsov").
 
 -behaviour(application).
 
